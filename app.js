39e6e79ae7d229f2cd18f708b690c766d43635cc
oliverbskau <oliverbskau@gmail.com>
2022-02-19 14:25:48 +0100

    first msg

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/TimerTroll.html");
+});
+
+const PORT = 8080;
+app.listen(process.env.PORT || "5000", (error) => {
+  console.log("Server is running on port", PORT);
+});
+//......
