b05ba5f8950ca9081a067446d8951cfef8fdda3b
oliverbskau <oliverbskau@gmail.com>
2022-02-19 15:21:50 +0100

    combined

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/TimerTroll.html");
 });
 
+
 const PORT = 8080;
 app.listen(process.env.PORT || "5000", (error) => {
   console.log("Server is running on port", PORT);
 });
+
 //......
